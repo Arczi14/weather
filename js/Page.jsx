266ffac3dbc54113d1cf20372de5e7370aaaefbb
Ścibior Arkadiusz <ascibior@kbj.com.pl>
2019-04-15 10:18:09 +0200

    fix geolokalizacji

--- js/Page.jsx
+++ js/Page.jsx
@@ -32,31 +32,12 @@ class Page extends React.Component {
             navigator.geolocation.getCurrentPosition(position => {
                 let latitude = position.coords.latitude.toFixed(4);
                 let longitude = position.coords.longitude.toFixed(4);
-                this.setState({
-                    GeoLatitudes: latitude,
-                    GeoLongitudes: longitude,
-                    geoloc: true
-                }), () => {fetch(`https://api.apixu.com/v1/current.json?key=7aeed42ae9eb4f47bc8185433170310&q=${this.state.GeoLatitudes},${this.state.GeoLongitudes}`).then( resp => resp.json() ).then( response => {
-                            console.log(response);
-                            const city = response.location.name
-                            const temp = response.current.temp_c
-                            const wind = response.current.wind_kph
-                            const clouds = response.current.cloud
-                            const preasure = response.current.pressure_mb
-
-                            this.setState({
-                                city: city,
-                                temp: temp,
-                                wind: wind,
-                                clouds: clouds,
-                                preasure: preasure
-                            })
-                        })
-                    }
-                })
-            }
-            console.log(`https://api.apixu.com/v1/current.json?key=7aeed42ae9eb4f47bc8185433170310&q=${this.state.GeoLatitudes},${this.state.GeoLongitudes}`);
+                console.log('latitude', latitude)
+                let url = `https://api.apixu.com/v1/current.json?key=7aeed42ae9eb4f47bc8185433170310&q=${latitude},${longitude}`
+                this.handleClick(undefined, url)
+            })
         }
+    }
 
     geolocClick = (event) => {
         this.findMe();
@@ -66,14 +47,21 @@ class Page extends React.Component {
         this.setState({name: event.target.value});
     }
 
-    handleClick = (event) => {
-        event.preventDefault();
+    handleClick = (event,url) => {
+        if (event) {
+            event.preventDefault();
+        }
 
-        let urlInput = `https://api.apixu.com/v1/current.json?key=7aeed42ae9eb4f47bc8185433170310&q=${this.state.name}`
+        let urlInput = ""
+        if (url) {
+            urlInput = url
+        } else {
+            urlInput = `https://api.apixu.com/v1/current.json?key=7aeed42ae9eb4f47bc8185433170310&q=${this.state.name}`
+        }
         this.setState({
             inputValue: this.state.name,
         });
-        console.log(urlInput);
+
         fetch(urlInput).then( resp => resp.json() ).then( response => {
             const city = response.location.name
             const temp = response.current.temp_c
